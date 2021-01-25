c47cbe8f03009227afc497d7a3f6e01762aa8c2e
Cuong Nguyen <cuong.tan@shopee.com>
2021-01-25 20:35:59 +0800

    Initial Commit

---
+++ MarkdownViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MarkdownViewer
+//
+//  Created by Nguyen Tan Cuong on 25/1/21.
+//  Copyright © 2021 Nguyen Tan Cuong. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MarkdownViewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MarkdownViewer
+//
+//  Created by Nguyen Tan Cuong on 25/1/21.
+//  Copyright © 2021 Nguyen Tan Cuong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
